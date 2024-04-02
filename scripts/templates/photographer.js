56cc65f436bd52e98656f057693b4e7469c76ce7
marwa mezghani <marwamezghani29@gmail.com>
2024-04-02 20:15:26 +0100

    config de eslint (installation de node et modification des config de eslint)
    recuperation des donnes par fetch d'un fichier json
    ajout des images
    ajout de template dans le html pour implémenter les donner de chaque photographer

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,31 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city,country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        // const article = document.createElement( 'article' );
+        // const img = document.createElement( 'img' );
+        // img.setAttribute("src", picture)
+        // const h2 = document.createElement( 'h2' );
+        // h2.textContent = name;
+        // article.appendChild(img);
+        // article.appendChild(h2);
+        // return (article);
+        const template = document.getElementById('template');
+        const clone = document.importNode(template.content,true);
+        const img = clone.querySelector('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        const h2 = clone.querySelector('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const pC = clone.querySelector('.cardCity');
+        pC.textContent = `${city}, ${country}`; 
+        const pT = clone.querySelector('.cardTagline');
+        pT.textContent = tagline;
+        const pP = clone.querySelector('.cardPrice');
+        pP.textContent = `${price}€/jour`;
+        return(clone)
     }
     return { name, picture, getUserCardDOM }
 }
