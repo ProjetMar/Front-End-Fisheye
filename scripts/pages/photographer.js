40c3ee89c6e82f4021acfde9c17260cf06425dde
marwa mezghani <marwamezghani29@gmail.com>
2024-04-07 12:57:09 +0200

    ajout de fichier Media.js qui contient le factoryMedia qui permet soit de creer new class Media image ou new class Media video et ces class renvoi les card media
    ajout de style pour les card
    ajout de fonction qui permet d'afficher les media dans la page photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,6 +9,9 @@ async function getPhotographers() {
     const photographers = await reponse.json();
     return(photographers);
 }
+
+const mainDOM = document.getElementById("main"); 
+
 async function displayData(photographers){
     const photographersSection = document.querySelector(".photograph-header");
     const photographContactBouton = document.querySelector(".contact_button");
@@ -23,10 +26,49 @@ async function displayData(photographers){
     photographersSection.appendChild(userImgDOM);
 }
 
+async function displayMedia(photographers, media){
+
+    const sectionMedia = document.createElement('section');
+    const photographer = photographers.find((photographer) => photographer.id === parseInt(Id));
+    sectionMedia.classList.add = "section-media";
+
+    mainDOM.appendChild(sectionMedia);
+    const mediaPhotographer = media.filter((medias)=>medias.photographerId === parseInt(Id));
+    const namePhotographer = photographer.name;
+    // const Tarif = photographer.price;
+    console.log(mediaPhotographer);
+
+    // let SommeLike = 0;
+    // mediaPhotographer.forEach((media)=>
+    // {
+    //     SommeLike+=media.likes;
+    // });
+    // console.log("la somme des likes est" + SommeLike);
+
+    mediaPhotographer.forEach((ElementMediaPhotographer) => {
+        const mediaModel = new MediaFactory(ElementMediaPhotographer, namePhotographer);
+        const mediaCardDOM = mediaModel.getMediaCardDOM();
+        sectionMedia.appendChild(mediaCardDOM);
+    })
+    // mainDOM.appendChild(getTarifLikes(SommeLike,Tarif)); 
+}
+
 async function init() {
     // Récupère les datas des photographes
     const { photographers } = await getPhotographers();
     displayData(photographers);
+    const { media } = await getPhotographers();
+    displayMedia(photographers, media);
+    let videoBlock = document.querySelector('.video-block');
+    console.log(videoBlock);
+    videoBlock.addEventListener('click',()=>{
+        let video=document.querySelector('video');
+        if (video.paused){
+            video.play();
+        }else{
+            video.pause();
+        }
+    }); 
 }
 
 init();
---
+++ scripts/templates/Media.js
@@ -0,0 +1,78 @@
+class MediaFactory {
+    constructor(data, namePhotographer){
+        if (Object.hasOwn(data, 'image')){
+            return new MediaImage (data, namePhotographer)
+        }else if((Object.hasOwn(data, 'video'))){
+            return new MediaVideo (data, namePhotographer)
+        }
+    }
+}
+class MediaImage {
+    constructor(data, namePhotographer){
+        this.title = data.title;
+        this.likes = data.likes;
+        this.image = data.image;
+        this.namePhotographer = namePhotographer;  
+    }
+    get picture(){
+        return `assets/images/${this.namePhotographer.split(' ')[0].replace('-',' ')}/${this.image}`
+    }
+    template(idTemplate){
+        const template = document.getElementById(idTemplate);
+        const clone = document.importNode(template.content,true);
+        return(clone)
+    }
+    getMediaCardDOM(){
+        const clone = this.template("CardImageMedia");
+
+        const img = clone.querySelector(".media-card-img");
+        img.setAttribute("src", this.picture);
+        img.setAttribute("alt", this.title);
+
+        const p = clone.querySelector(".TitleLike-media").firstElementChild;
+        p.textContent = this.title;
+
+        const pNumbreLike = clone.querySelector(".Likes-media").firstElementChild;
+        pNumbreLike.textContent = this.likes;
+
+        return(clone)
+    }
+}
+
+class MediaVideo {
+    constructor(data, namePhotographer){
+        this.title = data.title;
+        this.likes = data.likes;
+        this.video = data.video;
+        this.namePhotographer = namePhotographer;
+    }
+    get picture(){
+        return `assets/images/${this.namePhotographer.split(' ')[0].replace('-',' ')}/${this.video}`
+    }
+    get vigetteVideo(){
+        return`${this.picture.replace('.mp4','.jpg')}`
+    }
+    template(idTemplate){
+        const template = document.getElementById(idTemplate);
+        const clone = document.importNode(template.content,true);
+        return(clone)
+    }
+    getMediaCardDOM(){
+        const clone = this.template("CardVideoMedia");
+
+        const img = clone.querySelector("img");
+        img.setAttribute("src", this.vigetteVideo);
+        img.setAttribute("alt", `vignette de la video ${this.title}`);
+
+        const video = clone.querySelector("video");
+        video.setAttribute("src", this.picture);
+
+        const p = clone.querySelector(".TitleLike-media").firstElementChild;
+        p.textContent = this.title;
+
+        const pNumbreLike = clone.querySelector(".Likes-media").firstElementChild;
+        pNumbreLike.textContent = this.likes;
+
+        return(clone)
+    }
+}
